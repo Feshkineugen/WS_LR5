6702da9fae65aed82e1b9602da8680191a711122
Feshkineugen <118047190+Feshkineugen@users.noreply.github.com>
2024-04-17 15:00:35 +0300

    Add files via upload

---
+++ WS5/WS5/Horse.cs
@@ -0,0 +1,14 @@
+﻿class Horse : Animal
+{
+    public string color;
+
+    public override void MakeNoise()
+    {
+        Console.WriteLine("Лошадь ржёт.");
+    }
+
+    public override void Eat()
+    {
+        Console.WriteLine("Лошадь ест сено.");
+    }
+}
---
+++ WS5/WS5/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Dog dog = new Dog();
+        dog.food = "мясо";
+        dog.location = "будка";
+        dog.breed = "такса";
+
+        Cat cat = new Cat();
+        cat.food = "молоко";
+        cat.location = "квартира";
+        cat.livesLeft = 7;
+
+        Horse horse = new Horse();
+        horse.food = "сено";
+        horse.location = "пастбище";
+        horse.color = "чёрный";
+
+        Veterinarian vet = new Veterinarian();
+        vet.TreatAnimal(dog);
+        vet.TreatAnimal(cat);
+        vet.TreatAnimal(horse);
+    }
+}
